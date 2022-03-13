7367453655ed8259f0b99fa3e4f77ba0fb27f839
Amine Sebastian <amine@suburbandigital.net>
2022-03-13 03:59:53 -0400

    Updated version

--- app/services/user-data-access.js
+++ app/services/user-data-access.js
@@ -8,54 +8,52 @@ var main_1 = require("../main");
 var UserData_1 = require("../models/UserData");
 var keytar = require('keytar');
 var UserDataAccess = /** @class */ (function () {
-  function UserDataAccess() {
-    var _this = this;
-    if (fs.existsSync(electron_1.app.getPath('userData').concat('\\user-data.json'))) {
-      var data = fs.readFileSync(electron_1.app.getPath('userData').concat('\\user-data.json'), 'utf8');
-      this.cachedData = new UserData_1.UserData(JSON.parse(data));
+    function UserDataAccess() {
+        var _this = this;
+        if (fs.existsSync(electron_1.app.getPath('userData').concat('\\user-data.json'))) {
+            var data = fs.readFileSync(electron_1.app.getPath('userData').concat('\\user-data.json'), 'utf8');
+            this.cachedData = new UserData_1.UserData(JSON.parse(data));
+        }
+        electron_1.ipcMain.on('sparrow/user-data/update', function (event, data) {
+            _this.cachedData = data;
+            fs.writeFileSync(electron_1.app.getPath('userData').concat('\\user-data.json'), JSON.stringify(_this.cachedData, null, 4));
+        });
+        electron_1.ipcMain.on('sparrow/user-data', function (event) {
+            event.sender.send('sparrow/user-data', _this.cachedData);
+        });
+        electron_1.ipcMain.on('sparrow/open-raven-login', function (event, data) {
+            _this.openRavenLogin('http://localhost:4200/', 475, 675);
+        });
     }
-    electron_1.ipcMain.on('sparrow/user-data/update', function (event, data) {
-      _this.cachedData = data;
-      fs.writeFileSync(electron_1.app.getPath('userData').concat('\\user-data.json'), JSON.stringify(_this.cachedData, null, 4));
-    });
-    electron_1.ipcMain.on('sparrow/user-data', function (event) {
-      event.sender.send('sparrow/user-data', _this.cachedData);
-    });
-    electron_1.ipcMain.on('sparrow/open-raven-login', function (event, data) {
-      _this.openRavenLogin('http://localhost:4200/', 475, 675);
-    });
-  }
-  UserDataAccess.prototype.openRavenLogin = function (url, w, h) {
-    var electronScreen = electron_1.screen;
-    var size = electronScreen.getPrimaryDisplay().workAreaSize;
-    var y = main_1.win.getSize()[0] / 2 + main_1.win.getPosition()[0] - (h / 2);
-    var x = main_1.win.getSize()[1] / 2 + main_1.win.getPosition()[1] - (w / 2);
-    var popup = new electron_1.BrowserWindow({
-      x: x,
-      y: y,
-      width: w,
-      height: h,
-      webPreferences: {
-        allowRunningInsecureContent: false
-      }
-    });
-    // Load the url.
-    popup.loadURL(url);
-    popup.webContents.addListener('did-navigate-in-page', function (e, url) {
-      if (url.indexOf('#') > 0) {
-        var response = Buffer.from(url.split('#')[1], 'base64').toString();
-        console.log(response);
-        var token = JSON.parse(response).accessToken;
-        console.log(token);
-        var decodedToken = (0, jwt_decode_1.default)(token);
-        console.log(decodedToken);
-        keytar.setPassword('Raven', decodedToken.RavenId, response);
-        popup.close();
-      }
-    });
-    return popup;
-  };
-  return UserDataAccess;
+    UserDataAccess.prototype.openRavenLogin = function (url, w, h) {
+        var electronScreen = electron_1.screen;
+        var size = electronScreen.getPrimaryDisplay().workAreaSize;
+        var y = main_1.win.getSize()[0] / 2 + main_1.win.getPosition()[0] - (h / 2);
+        var x = main_1.win.getSize()[1] / 2 + main_1.win.getPosition()[1] - (w / 2);
+        var popup = new electron_1.BrowserWindow({
+            x: x,
+            y: y,
+            width: w,
+            height: h,
+            webPreferences: {
+                allowRunningInsecureContent: false
+            }
+        });
+        // Load the url.
+        popup.loadURL(url);
+        popup.webContents.addListener('did-navigate-in-page', function (e, url) {
+            // Once navigated, grab the encoded token from the URL and store it in our credential manager under the user's ravenId.
+            if (url.indexOf('#') > 0) {
+                var response = Buffer.from(url.split('#')[1], 'base64').toString();
+                var token = JSON.parse(response).accessToken;
+                var decodedToken = (0, jwt_decode_1.default)(token);
+                keytar.setPassword('Raven', decodedToken.RavenId, response);
+                popup.close();
+            }
+        });
+        return popup;
+    };
+    return UserDataAccess;
 }());
 exports.UserDataAccess = UserDataAccess;
 //# sourceMappingURL=user-data-access.js.map
